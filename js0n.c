f150a1c243b1174600fef44cb217d4991e96f6a1
Matthijs Kooijman <matthijs@stdin.nl>
2014-03-16 22:38:19 +0100

    Let js0n accept a const char pointer

--- js0n.c
+++ js0n.c
@@ -9,7 +9,7 @@
 #define PUSH(i) if(depth == 1) prev = *out++ = ((cur+i) - js)
 #define CAP(i) if(depth == 1) prev = *out++ = ((cur+i) - (js + prev) + 1)
 
-int js0n(unsigned char *js, unsigned int len, unsigned short *out, unsigned int olen)
+int js0n(const unsigned char *js, unsigned int len, unsigned short *out, unsigned int olen)
 {
 	unsigned short prev = 0, *oend;
 	unsigned char *cur, *end;
--- js0n.h
+++ js0n.h
@@ -1,3 +1,3 @@
 // pass it a raw json string and length, and it will tag all the key/value offsets in the out argument (size it same as js to be safe)
 // returns 0 if successful, >0 if not
-int js0n(unsigned char *js, unsigned int len, unsigned short *out, unsigned int olen);
+int js0n(const unsigned char *js, unsigned int len, unsigned short *out, unsigned int olen);
