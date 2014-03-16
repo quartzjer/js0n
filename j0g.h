04e88f0e90fa3ca3b62620a0e47595ddd5a8e0ab
Matthijs Kooijman <matthijs@stdin.nl>
2014-03-16 22:48:03 +0100

    Add const specifiers to j0g_* functions

--- j0g.h
+++ j0g.h
@@ -11,13 +11,13 @@ char *j0g(const char *json, unsigned short *index, int ilen);
 // these functions will modify the json arg in place (to null terminate strings)
 
 // return the null-terminated string value matching the given key
-char *j0g_str(char *key, char *json, unsigned short *index);
+char *j0g_str(const char *key, char *json, const unsigned short *index);
 
 // return 1 if the value is the bool value true, number 1, or even the string "true", false otherwise
-int j0g_test(char *key, char *json, unsigned short *index);
+int j0g_test(const char *key, char *json, const unsigned short *index);
 
 // return the index offset of the value matching the given key
-int j0g_val(char *key, char *json, unsigned short *index);
+int j0g_val(const char *key, char *json, const unsigned short *index);
 
 // unescapes and null terminates any value (useful for arrays)
-char *j0g_safe(int val, char *json, unsigned short *index);
+char *j0g_safe(int val, char *json, const unsigned short *index);
